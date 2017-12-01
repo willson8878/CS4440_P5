d80e9c1bf6f685dd2ff3dab59e9ac2cc132af485
Will <wchen8878@gmail.com>
2017-11-30 19:19:23 -0500

    add main.js

--- main.js
+++ main.js
@@ -1,4 +1,20 @@
-// Global function called when select element is changed
+var svg = d3.select('svg');
+
+
+width = +svg.attr("width"),
+height = +svg.attr("height");
+
+var format = d3.format(",d");
+
+var valueColors = ['#468269','#00fffe','#0000fe','#ff00fe', '#fffc00', '#ff0000', '#e18231'];
+
+var pack = d3.pack()
+.size([800, 800])
+.padding(1.5);
+
+///////////////////////////////////////   Bubble   ////////////////////////////////////////////////////////////////////////////////////////////
+//////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+/////////////////////////////////////////// Dot  ////////////////////////////////////////////////////////////////////////////////////////////////////////
 
 var xDomain;
 var yDomain;
@@ -16,12 +32,8 @@ function onCategoryChanged() {
     updateChart(xDomain, yDomain);
 }
 
-
-var svg = d3.select('svg');
-
 var data;
 
-// Create a group element for appending chart elements
 var chartG = svg.append('g');
 
 var xAxisG = chartG.append('g')
@@ -36,22 +48,86 @@ var transitionScale = d3.transition()
                         .duration(600)
                         .ease(d3.easeLinear);
 
-d3.csv('./data/colleges.csv', 
-    function(error, dataset) {
-        if(error) {
-            console.error('Error while loading ./colleges.csv dataset.');
-            console.error(error);
-            return;
-        } 
+/////////////////////////////////////////// Dot  ////////////////////////////////////////////////////////////////////////////////////////////////////////
+//////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+//////////////////////   Bubble   //////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
-    data = dataset;
+
+d3.csv("./data/colleges.csv", function(d) {
+    d.value = +d.UndergradPopulation;
+    d.local = d.Locale
+    if (d.value) return d;
+  }, function(error, classes) {
+    if (error) throw error;
+  
+    var root = d3.hierarchy({children: classes})
+        .sum(function(d) { return d.value; })
+        .sort(function(a, b) { return b.value - a.value; })
+        .each(function(d) {
+          if (name = d.data.Name) {
+            var name, i = name.lastIndexOf(".");
+            d.name = name;
+            d.package = name.slice(0, i);
+            d.class = name.slice(i + 1);
+          }
+        });
+  
+    var node = svg.selectAll(".node")
+      .data(pack(root).leaves())
+      .enter().append("g")
+        .attr("class", "node")
+        .attr("transform", function(d) { return "translate(" + d.x + "," + d.y + ")"; });
+  
+    node.append("circle")
+        .attr("id", function(d) { return d.name; })
+        .attr("r", function(d) { return d.r; })
+        .style("fill", function(d) {
+            if (d.data.Locale == "Mid-size City") {
+                return valueColors[0];
+            } else if (d.data.Locale == "Remote Town") {
+                return valueColors[1];
+            } else if (d.data.Locale == "Large Suburb") {
+                return valueColors[2];
+            } else if (d.data.Locale == "Distant Town") {
+                return valueColors[3];
+            } else if (d.data.Locale == "Small City") {
+                return valueColors[4];
+            } else if (d.data.Locale == "Fringe Town") {
+                return valueColors[5];
+            } else if (d.data.Locale == "Large City") {
+                return valueColors[6];
+            }
+        });
+  
+    node.append("clipPath")
+        .attr("id", function(d) { return "clip-" + d.name; })
+      .append("use")
+        .attr("xlink:href", function(d) { return "#" + d.name; });
+  
+    node.append("text")
+        .attr("dy", ".3em")
+        .style("text-anchor", "middle")
+        .text(function(d) { return d.name.substring(0, d.r / 3); });
+  
+    node.append("title")
+        .text(function(d) { return d.name + "\n" + format(d.value); });
+
+
+
+
+
+/////////////////////////   Bubble   ///////////////////////////////////////////////////////////////////////////////////////////////////////////
+//////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+///////////////////////////////////////// Dot  ////////////////////////////////////////////////////////////////////////////////
+
+    data = classes;
     // x-axis
     xScale = d3.scaleLinear().range([900, 1400]);
     yScale = d3.scaleLinear().range([500, 0]);
 
     updateChart('Admission Rate','Admission Rate');
         
-    });
+});
 
 function updateChart(x, y){
 
@@ -136,9 +212,10 @@ function updateChart(x, y){
             if(d.Region == 'Southeast') return '#5D8896';
             if(d.Region == 'Southwest') return '#ACC4E6';
         });
-                    
-
 }
+////////////////////// Dot  ////////////////////////////////////////////////////////////////////////////////////////////////////////////
+//////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+//////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
 
 
